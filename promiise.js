3449efe56852b890b04c817a9433f8aaaeb8f3d1
Bhushan Mistari <mistaribhushan405@gmail.com>
2026-02-01 11:29:11 +0530

    Refactor promise handling and add error logging for multiple promises

--- promiise.js
+++ promiise.js
@@ -12,12 +12,23 @@ let prom1 = new Promise((resolve, reject) => {
   }
 });
 
-prom1
-  .then((a) => {
-    console.log(a);
-  })
-  .catch((err) => {
-    console.log(err
-        
-    );
-  });
+let prom2 = new Promise((resolve, reject) => {
+  let a = Math.random();
+  if (a < 0.5) {
+    reject("No random number was not supporting you");
+  } else {
+    setTimeout(() => {
+      console.log("Yes i am done 2");
+      resolve("yash 2");
+    }, 1000);
+  }
+});
+
+
+// Promise.all  Promise.race  Promise.allSettled  Promise.resolve
+let p3 = Promise.reject([prom1, prom2]);
+p3.then((a) => {
+  console.log(a);
+}).catch((err) => {
+  console.log(err);
+});
